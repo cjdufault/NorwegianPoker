c415a6fac7862acd10732897b963f0f4dd3b5f16
cjdufault <dufaultconnor@gmail.com>
2020-01-20 16:37:31 -0600

    added dice images, cards are now dealt

--- norwegian_poker.py
+++ norwegian_poker.py
@@ -1,22 +1,33 @@
 import os
 
 import pygame
-import time
 import np_classes
+import time
 
 
 def main():
     pygame.init()
 
     # create screen, set video mode
-    screen = pygame.display.set_mode((1024, 576))
+    screen_width = 1024
+    screen_height = 576
+    screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Norwegian Poker")
 
     # load assets
-    background = pygame.image.load("assets/poker_table.jpg").convert()
-    title_image = pygame.image.load("assets/np_title.png").convert()
+    background = pygame.image.load(os.path.join("assets", "poker_table.jpg")).convert()
+    title_image = pygame.image.load(os.path.join("assets", "np_title.png")).convert()
+    dice1_image = pygame.image.load(os.path.join("assets", "dice", "dice1.png"))
+    dice2_image = pygame.image.load(os.path.join("assets", "dice", "dice2.png"))
+    dice3_image = pygame.image.load(os.path.join("assets", "dice", "dice3.png"))
+    dice4_image = pygame.image.load(os.path.join("assets", "dice", "dice4.png"))
+    dice5_image = pygame.image.load(os.path.join("assets", "dice", "dice5.png"))
+    dice6_image = pygame.image.load(os.path.join("assets", "dice", "dice6.png"))
 
-    # draw assets
+    dice_images = [dice1_image, dice2_image, dice3_image, dice4_image, dice5_image, dice6_image]
+    dice_origns = (((screen_width / 2) - 65, (screen_height / 2) - 30), ((screen_width / 2) + 65, (screen_height / 2) - 30))
+
+    # draw background and title
     screen.blit(background, (0, 0))
     screen.blit(title_image, (192, 75))
     pygame.display.update()
@@ -33,27 +44,45 @@ def main():
             pygame.display.update(title_rect)
             intro_done = True
 
-    player1 = np_classes.Player("clubs", (307, 374), False)
-    player2 = np_classes.Player("diamonds", (, ), True)
-    player3 = np_classes.Player("hearts", (307, 20), False)
-    player4 = np_classes.Player("spades", (, ), True)
+    player1 = np_classes.Player("clubs", ((screen_width / 2) - 205, screen_height - 202), False, True)
+    player2 = np_classes.Player("diamonds", (20, (screen_height / 2) - 283), True, False)
+    player3 = np_classes.Player("hearts", ((screen_width / 2) - 205, 20), False, False)
+    player4 = np_classes.Player("spades", (screen_width - 150, (screen_height / 2) - 283), True, False)
 
+    deal(screen, [player1, player2, player3, player4])
     running = True
 
     while running:
-        deal(screen, [player1, player2, player3, player4])
-
         event = pygame.event.wait()
-        # print(str(event.type) + " - " + pygame.event.event_name(event.type))
+
         if event.type == pygame.QUIT:
             running = False
         # TODO: add listeners for different actions
 
 
 def deal(screen, players):
-    testimage = pygame.image.load("assets/clubs/2.png")
-    screen.blit(testimage, (0, 0))
-    pygame.display.update()
+    for player in players:
+        origin = player.get_origin()
+        orig_x = origin[0]
+        orig_y = origin[1]
+
+        # draw all of the cards
+        if player.get_is_vert():
+            for i in range(5):
+                screen.blit(player.get_card(i + 2).get_image(), (orig_x, orig_y + (i * 96)))
+                screen.blit(player.get_card(i + 8).get_image(), (orig_x + 70, orig_y + (i * 96)))
+                pygame.display.update(pygame.Rect(orig_x, orig_y, orig_x + 130, orig_y + 470))
+                pygame.time.wait(100)
+        else:
+            for i in range(5):
+                screen.blit(player.get_card(i + 2).get_image(), (orig_x + (i * 70), orig_y))
+                screen.blit(player.get_card(i + 8).get_image(), (orig_x + (i * 70), orig_y + 96))
+                pygame.display.update(pygame.Rect(orig_x, orig_y, orig_x + 340, orig_y + 182))
+                pygame.time.wait(100)
+
+
+# def roll(screen, dice_images):
+
 
 
 if __name__ == '__main__':
--- np_classes.py
+++ np_classes.py
@@ -4,15 +4,15 @@ import pygame
 
 
 class Player:
-    def __init__(self, suit, origin, vert_bool):
+    def __init__(self, suit, origin, is_vert, is_human):
         self.suit = suit  # the suit that the player is playing with
         self.origin = origin    # indicates the location where the player's hand is to be drawn
-        self.vert_bool = vert_bool  # indicates the orientation that the player's hand is to be drawn in
+        self.is_vert = is_vert  # indicates the orientation that the player's hand is to be drawn in
+        self.is_human = is_human
 
         path = os.path.join("assets", suit)
-        self.hand = {1: Card("A", path), 2: Card("2", path), 3: Card("3", path), 4 : Card("4", path), 5: Card("5", path),
-                     6: Card("6", path), 8: Card("8", path), 9: Card("9", path), 10: Card("10", path), 11: Card("J", path),
-                     12: Card("Q", path)}
+        self.hand = {2: Card("2", path), 3: Card("3", path), 4: Card("4", path), 5: Card("5", path), 6: Card("6", path),
+                     8: Card("8", path), 9: Card("9", path), 10: Card("10", path), 11: Card("J", path), 12: Card("Q", path)}
 
     def flip_card(self, dice_roll):
         self.hand[dice_roll].flip()
@@ -20,28 +20,34 @@ class Player:
     def get_card(self, card_int):
         return self.hand[card_int]
 
+    def get_hand(self):
+        return self.hand
+
     def get_origin(self):
         return self.origin
 
-    def get_vert_bool(self):
-        return self.vert_bool
+    def get_is_vert(self):
+        return self.is_vert
+
+    def get_is_human(self):
+        return self.is_human
 
 
 class Card:
     def __init__(self, card_name, image_path):
         self.card_name = card_name
-        self.face_image = pygame.image.load(os.path.join(image_path, card_name + ".png")).convert()
-        self.back_image = pygame.image.load(os.path.join("assets", "card_back.png")).convert()
+        self.image = pygame.image.load(os.path.join(image_path, card_name + ".png")).convert()
         self.face_up = True
-
-    def get_current_image(self):
-        if self.face_up:
-            return self.face_image
-        else:
-            return self.back_image
 
     def flip(self):
         if self.face_up:
             self.face_up = False
         else:
             self.face_up = True
+
+    def get_face_up(self):
+        if self.face_up:
+            return True
+
+    def get_image(self):
+        return self.image
