21333e9d4a510c4a351795cbbd2f06edc156012d
cjdufault <dufaultconnor@gmail.com>
2020-02-09 07:43:01 -0600

    eliminated duplicated code used to wait for a given number of milliseconds so long as a quit event hasn't been registered

--- norwegian_poker.py
+++ norwegian_poker.py
@@ -44,9 +44,7 @@ def do_turn():
 
     # for computer players
     if not human:
-        listen_for_quit()
-        if running:
-            pygame.time.wait(500)
+        wait(500)
 
         listen_for_quit()
         if running:
@@ -55,9 +53,7 @@ def do_turn():
             result = dice_roll[0] + dice_roll[1]
             dice_rolled = True
 
-        listen_for_quit()
-        if running:
-            pygame.time.wait(500)
+        wait(500)
 
     # for human players
     else:
@@ -102,9 +98,7 @@ def do_turn():
             for i in range(len(display.players)):
                 turn = increment_turn(turn, len(display.players))
 
-                listen_for_quit()
-                if running:
-                    pygame.time.wait(300)
+                wait(300)
 
                 if not other_player_has_card_face_up:
                     next_player_has_card = display.players[turn].get_card(result).is_face_up()
@@ -114,36 +108,27 @@ def do_turn():
                         display.flip_card(display.players[turn], result)
                         other_player_has_card_face_up = True
 
-                        listen_for_quit()
-                        if running:
-                            pygame.time.wait(500)
+                        wait(500)
                         break
 
             if not other_player_has_card_face_up:
                 display.flip_card(player, result)
 
-                listen_for_quit()
-                if running:
-                    pygame.time.wait(500)
-                    turn = increment_turn(turn, len(display.players))
+                wait(500)
+                turn = increment_turn(turn, len(display.players))
 
     # there are no sevens in this game, so we just go to the next turn
     elif dice_rolled and result == 7:
         turn = increment_turn(turn, len(display.players))
 
-        listen_for_quit()
-        if running:
-            pygame.time.wait(500)
+        wait(500)
 
     # tests if the game has ended
     for p in display.players:
         if p.has_won():
 
             # pause for long enough for the player to see the game is over
-            listen_for_quit()
-            if running:
-                pygame.time.wait(1000)
-                game_over(p.get_suit())
+            wait(1000)
 
 
 def roll():
@@ -190,6 +175,12 @@ def listen_for_quit():
     quit_events = pygame.event.get(pygame.QUIT)
     if len(quit_events) > 0:
         running = False
+
+
+def wait(time):
+    listen_for_quit()
+    if running:
+        pygame.time.wait(time)
 
 
 if __name__ == '__main__':
